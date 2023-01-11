5f99ddb89ae8569d15343791059f653e00239c59
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-01-11 09:30:12 +0100

    Add files via upload

---
+++ KLasaString2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLasaString2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst1 = "Ovo je neki tekst.";
+            string tekst2 = "Ovo je nek tekst.";
+            string tekst3 = "Ovo je neki tekst.";
+            string tekst4 = "Ovo je neki ekst.";
+
+            Console.WriteLine(string.Compare(tekst1,tekst2));
+            if(tekst1 == tekst2)
+            {
+                Console.WriteLine("Tekstovi su isti");
+            }
+            else
+            {
+                Console.WriteLine("Tekstovi nisu isti");
+            }
+            Console.WriteLine(string.Compare(tekst1, tekst3));
+            if (tekst1 == tekst3)
+            {
+                Console.WriteLine("Tekstovi su isti");
+            }
+            else
+            {
+                Console.WriteLine("Tekstovi nisu isti");
+            }
+            Console.WriteLine(string.Compare(tekst1, tekst4));
+            if (tekst1 == tekst4)
+            {
+                Console.WriteLine("Tekstovi su isti");
+            }
+            else
+            {
+                Console.WriteLine("Tekstovi nisu isti");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
