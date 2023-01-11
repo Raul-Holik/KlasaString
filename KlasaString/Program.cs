79c1bb783e9c01d31755c0eb702c62536b996c66
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-01-11 09:06:43 +0100

    Add files via upload

---
+++ KlasaString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica = "Danas radimo klasu String.";
+
+            //ispis broja znakova
+            Console.WriteLine("Broj znakova u recenici: {0}", recenica.Length);
+            //ispis velikim i malim slovima
+            Console.WriteLine("Ispis velikim slovima: {0}\nIspis malim slovima: {1}", recenica.ToUpper(), recenica.ToLower());
+            //ispis određene rijeci
+            Console.WriteLine(recenica.Substring(0, 5));
+            Console.WriteLine(recenica.Substring(recenica.Length-5,5));
+            //zamjena znakova
+            Console.WriteLine(recenica.Replace("Danas", "Sutra"));
+            //razdvajanje stringova
+            string[]rijec=recenica.Split(' ');
+            for(int i=0;i<rijec.Length;i++)
+            {
+                Console.WriteLine(rijec[i]);
+            }
+            //umetanje znakova
+            Console.WriteLine(recenica.IndexOf("radimo"));
+            Console.WriteLine(recenica.Insert(recenica.IndexOf("radimo"), "intenzivno "));
+            Console.ReadKey();
+        }
+    }
+}
